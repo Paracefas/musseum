e2c7f943ca5655d57c3e088dcf65d3bfdccc594d
Emanuel Clur <emanuelclur2001@outlook.com>
2019-11-28 18:58:00 -0300

    POST / works

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.set('port', process.env.PORT || 27015)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.post('/', (req, res) =>
+{
+    const id = req.body.id
+    const json = require('./musseum.json')
+
+    res.json(json.musseum[id])
+})
+
+app.listen(app.get('port'), () => console.log(app.get('port')))
